c118697739a5702a5fe737d0e403c92351a93055
Dmitry Bakanov <61016327+DimaScientist@users.noreply.github.com>
2021-10-01 12:19:35 +0400

    fix print matrix

--- MatMul/kernel.cu
+++ MatMul/kernel.cu
@@ -14,7 +14,7 @@ using namespace std;
 void print_matrix(double* matrix, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << matrix[i + j] << " ";
+            cout << matrix[i * n + j] << " ";
         }
         cout << endl;
     }
